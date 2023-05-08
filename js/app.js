918168f164bb6182a47dd5d0e3cb183d0b8bfb02
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-07 23:15:53 -0400

    feat: Adds alert and validation of empty fields

--- js/app.js
+++ js/app.js
@@ -15,7 +15,22 @@ function guardarCliente() {
   const camposVacios = [mesa, hora].some((campo) => campo === "");
 
   if (camposVacios) {
-    console.log("hay campos vacios");
+    // verificar si existe alerta
+    const existeAlerta = document.querySelector(".invalid-feedback");
+    if (!existeAlerta) {
+      // creamos el elemento para el mensaje de alerta
+      const alerta = document.createElement("DIV");
+      alerta.classList.add("invalid-feedback", "d-block", "text-center");
+      alerta.textContent = "Todos los campos son obligatorios";
+
+      // agregamos la alerta en el html
+      document.querySelector(".modal-body form").appendChild(alerta);
+
+      // eliminamos el mensaje luego de notificar al usuario
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
   } else {
     console.log("campos validados");
   }
