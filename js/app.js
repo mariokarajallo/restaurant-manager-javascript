9270d651e3ba0d0f0960abf6f31cfc914365fc48
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-07 23:04:39 -0400

    feat: Add field validation in the modal

--- js/app.js
+++ js/app.js
@@ -8,5 +8,15 @@ const btnGuardarCliente = document.querySelector("#guardar-cliente");
 btnGuardarCliente.addEventListener("click", guardarCliente);
 
 function guardarCliente() {
-  console.log("guardando cliente");
+  const mesa = document.querySelector("#mesa").value;
+  const hora = document.querySelector("#hora").value;
+
+  // metodo some para revisar si hay campos vacios
+  const camposVacios = [mesa, hora].some((campo) => campo === "");
+
+  if (camposVacios) {
+    console.log("hay campos vacios");
+  } else {
+    console.log("campos validados");
+  }
 }
